207e25a7e2ef71d3460226c70897ec335f507262
Josh McKenna <joshmckenna@grace-bible.org>
2024-02-09 13:29:15 -0600

    Restructured command flow as functions

--- offboard.sh
+++ offboard.sh
@@ -1,119 +1,178 @@
 #!/bin/bash
 
+#Move execution to the script parent directory
 INITIAL_WORKING_DIRECTORY=$(pwd)
-
 parent_path=$(
     cd "$(dirname "${BASH_SOURCE[0]}")"
     pwd -P
 )
-
 cd "$parent_path"
 
-accountName=joshmckenna
-
-#Define today's date and time as a variable $NOW
+#Define variables
 NOW=$(date '+%F')
 
-#Define location for logs as a variable $logLocation
+accountName=$(whoami)
+
 logDirectory=/Users/joshmckenna/Library/CloudStorage/GoogleDrive-joshmckenna@grace-bible.org/Shared\ drives/IT\ subcommittee/_ARCHIVE/gam
+logFile=$logDirectory/$NOW.log
 
-if [ -d "$logDirectory" ]; then
-    echo "Logging to Google Drive File Stream via joshmckenna@grace-bible.org"
-    logLocation=$logDirectory/$NOW.log
-    echo
-elif [ ! -d "/Users/$accountName/GAMWork/logs/" ]; then
-    echo "Setting up Logs directory"
-    mkdir $logDirectory
-    echo "Logging to /Users/$accountName/GAMWork/logs directory"
-    logLocation=/Users/$accountName/GAMWork/logs/$NOW.log
+GAM3=/Users/$accountName/bin/gamadv-xtd3/gam
+
+if [[ -n "$1" ]] && [[-n "$2" ]]; then
+    offboard_user="$1"
+    receiving_user="$2"
 else
-    echo "Logging to /Users/$accountName/GAMWork/logs directory"
-    logLocation=/Users/$accountName/GAMWork/logs/$NOW.log
-fi
-
-(
-    GAM3=/Users/$accountName/bin/gamadv-xtd3/gam
-    echo "========================================"
-    echo "GAM3 command alias set to ${GAM3}"
-    echo
-    echo
-
+    echo "You ran the script without from and to emails!"
+    echo ""
     echo "Input the email address of the user to offboard from Google Workspace, followed by [ENTER]"
     read offboard_user
-    echo
-    echo
+    echo ""
+    echo ""
+    echo "Input the email address of the user to receive from $offboard_user, followed by [ENTER]"
+    read receiving_user
+    echo ""
+    echo ""
+fi
 
+#Define available functions for the Whiptail menu
+start_logger() {
+    exec &> >(tee -a "$logFile")
+    echo "========================================"
+    echo "GAM3 command alias set to ${GAM3}"
+}
+
+get_info() {
     echo "Logging user's pre-offboarding info for audit..."
     ${GAM3} info user $offboard_user
     ${GAM3} user $offboard_user show forwards
     echo
+}
+
+unsuspend() {
     echo "Unsuspending user account for offboarding..."
     ${GAM3} update user $offboard_user suspended off
-    echo
-    echo "Waiting 10 seconds for suspension to be removed..."
+    echo ""
+    echo "Waiting for suspension to be removed..."
     sleep 10
-    echo
+    echo ""
+}
+
+reset_password() {
     echo "Generating random password..."
     ${GAM3} update user $offboard_user password random
-    echo
-    echo "Hiding $offboard_user from the Global Address List (GAL)..."
-    ${GAM3} update user $offboard_user gal off #https://github.com/GAM-team/GAM/wiki/GAM3DirectoryCommands
-    echo
+    ${GAM3} update user $offboard_user changepassword on
+    sleep 2
+    ${GAM3} update user $offboard_user changepassword off
+    echo ""
+}
+
+reset_recovery() {
     echo "Erasing recovery options for $offboard_user..."
     ${GAM3} update user $offboard_user recoveryemail "" recoveryphone ""
-    echo
+    echo ""
+}
+
+set_endDate() {
     echo "Setting $offboard_user end date to today..."
-    ${GAM3} update user $offboard_user Employment_History.End_dates multivalued $NOW #https://github.com/GAM-team/GAM/wiki/GAM3DirectoryCommands#setting-custom-user-schema-fields-at-create-or-update
-    echo
+    ${GAM3} update user $offboard_user Employment_History.End_dates multivalued $NOW
+    echo ""
+    #https://github.com/GAM-team/GAM/wiki/GAM3DirectoryCommands#setting-custom-user-schema-fields-at-create-or-update
+}
+
+deprovsion() {
     echo "Deprovisioning application passwords, backup verification codes, and access tokens..."
     echo "Disabling POP/IMAP access, signing out all devices, and turning off MFA..."
-    ${GAM3} user $offboard_user deprovision popimap signout turnoff2sv #https://github.com/taers232c/GAMADV-XTD3/wiki/Users-Deprovision
+    ${GAM3} user $offboard_user deprovision popimap signout turnoff2sv
     ${GAM3} user $offboard_user update backupcodes
-    echo
-    echo
+    echo ""
+    #https://github.com/taers232c/GAMADV-XTD3/wiki/Users-Deprovision
+}
 
-    #echo "Do you want to transfer emails, calendars, and drive files to another user? (Y/N)"
-    #read transfer_choice
+remove_directory() {
+    echo "Hiding $offboard_user from the Global Address List (GAL)..."
+    ${GAM3} update user $offboard_user gal off
+    echo ""
+    #https://github.com/GAM-team/GAM/wiki/GAM3DirectoryCommands
+}
 
-    echo "Input the email address of the user to receive from $offboard_user, followed by [ENTER]"
-    read receiving_user
-    echo
-    echo
-
-    echo "Forwarding emails..."
-    company="Grace Bible Church"
-    subject="No longer at Grace Bible Church"
-    message="Thank you for contacting me. I am no longer working at $company. Please direct any future correspondence to $receiving_user."
+forward_emails() {
+    echo "Forwarding emails, granting delegate access to manager..."
     ${GAM3} user $offboard_user print forwardingaddresses | ${GAM3} csv - gam user "~User" delete forwardingaddress "~forwardingEmail"
     ${GAM3} user $offboard_user add forwardingaddress $receiving_user
-    ${GAM3} user $offboard_user forward on $receiving_user archive #https://github.com/taers232c/GAMADV-XTD3/wiki/Users-Gmail-Forwarding
-    echo
+    ${GAM3} user $offboard_user forward on $receiving_user archive
+    ${GAM3} user $offboard_user delegate to $receiving_user
+    echo ""
+    #https://github.com/taers232c/GAMADV-XTD3/wiki/Users-Gmail-Forwarding
+}
+
+set_autoreply() {
     echo "Configuring email autoreply..."
+    company="Grace Bible Church"
+    subject="No longer at $company"
+    message="Thank you for contacting me. I am no longer working at $company. Please direct any future correspondence to $receiving_user."
     startDate=$NOW
     endDate=$(date -v +1y +%F)
     ${GAM3} user $offboard_user vacation on subject "$subject" message "$message" startdate "$startDate" enddate "$endDate"
-    echo
+    echo ""
+}
+
+transfer_drive() {
     echo "Transferring Drive..."
-    ${GAM3} user $offboard_user transfer drive $receiving_user #https://github.com/taers232c/GAMADV-XTD3/wiki/Users-Drive-Transfer
-    echo
+    ${GAM3} user $offboard_user transfer drive $receiving_user
+    ${GAM3} create datatransfer $offboard_user gdrive $receiving_user
+    echo ""
+    #https://github.com/taers232c/GAMADV-XTD3/wiki/Users-Drive-Transfer
+}
+
+transfer_calendar() {
     echo "Transferring Calendar..."
     ${GAM3} calendar $offboard_user add owner $receiving_user
     ${GAM3} add datatransfer $offboard_user calendar $receiving_user releaseresources
-    echo
-    echo "Removing from groups..."
-    ${GAM3} user $offboard_user delete groups
-    echo
-    echo "Moving $offboard_user to Offboarding OU..."
-    ${GAM3} update org Inactive move user $offboard_user
-    echo
-    echo
+    echo ""
+}
+
+remove_drives() {
+    ${GAM3} redirect csv ./SharedDriveAccess.csv user $offboard_user shareddrives fields id,name
+    ${GAM3} redirect stdout ./DeleteSharedDriveAccess.txt multiprocess redirect stderr stdout csv ./SharedDriveAccess.csv gam delete drivefileacl ~id ~User
+}
+
+set_org_unit() {
+    echo "Moving $offboard_user to offboarding OU..."
+    ${GAM3} update org 'Inactive' move user $offboard_user
+    echo ""
+}
+
+suspend() {
     echo "Waiting for previous changes to take effect..."
     sleep 10
     ${GAM3} update user $offboard_user suspended on
+}
 
+endlogger() {
     echo "Google Workspace deprovisioning for $offboard_user complete"
     echo "========================================"
+}
 
-) 2>&1 | tee -a "$logLocation"
+#Start the global logger, begin functions
+start_logger
+unsuspend
 
+#get_info
+#reset_password
+#reset_recovery
+#set_endDate
+#deprovsion
+#remove_directory
+#forward_emails
+#set_autoreply
+#transfer_drive
+#transfer_calendar
+#remove_groups
+set_org_unit
+suspend
+endlogger
+
+#Return to the pre-script working directory
 cd $INITIAL_WORKING_DIRECTORY
+
+#home is ~, current is ./, parent is ../
