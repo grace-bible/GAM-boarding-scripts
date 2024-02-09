75bcb2dd4e88020365c630762e67f0b0cf658c1b
Josh McKenna <joshmckenna@grace-bible.org>
2024-02-09 16:18:26 -0600

    Restructured script as modular functions

--- onboard.sh
+++ onboard.sh
@@ -1,215 +1,185 @@
 #!/bin/bash
 
+#Move execution to the script parent directory
 INITIAL_WORKING_DIRECTORY=$(pwd)
-
 parent_path=$(
     cd "$(dirname "${BASH_SOURCE[0]}")"
     pwd -P
 )
-
 cd "$parent_path"
 
-accountName=joshmckenna
-
-#Define today's date and time as a variable $NOW
+#Define variables
 NOW=$(date '+%F')
 
-#Define location for logs as a variable $logLocation
+accountName=$(whoami)
+
 logDirectory=/Users/joshmckenna/Library/CloudStorage/GoogleDrive-joshmckenna@grace-bible.org/Shared\ drives/IT\ subcommittee/_ARCHIVE/gam
+logFile=$logDirectory/$NOW.log
 
-if [ -d "$logDirectory" ]; then
-    echo "Logging to Google Drive File Stream via joshmckenna@grace-bible.org"
-    logLocation=$logDirectory/$NOW.log
-    echo
-elif [ ! -d "/Users/$accountName/GAMWork/logs/" ]; then
-    echo "Setting up Logs directory"
-    mkdir $logDirectory
-    echo "Logging to /Users/$accountName/GAMWork/logs directory"
-    logLocation=/Users/$accountName/GAMWork/logs/$NOW.log
+GAM3=/Users/$accountName/bin/gamadv-xtd3/gam
+
+sigFile=/Users/joshmckenna/repos/GAM-boarding-scripts/dependencies/signature.txt
+onboard_manager=janineford@grace-bible.org,joshmckenna@grace-bible.org
+TEMP_PASS=P@ssw0rdy
+
+#Check for arguments
+if [[ -n "$1" ]] && [[-n "$2" ]] && [[-n "$3" ]] && [[-n "$4" ]] && [[-n "$5" ]] && [[-n "$6" ]]; then
+    onboard_first_name="$1"
+    onboard_last_name="$2"
+    onboard_user="$3"
+    recovery_email="$4"
+    campus="$5"
+    job_title="$6"
+    manager_email_address="$7"
 else
-    echo "Logging to /Users/$accountName/GAMWork/logs directory"
-    logLocation=/Users/$accountName/GAMWork/logs/$NOW.log
-fi
-
-#Make sure to change the path of your signature file below
-sigFile=/Users/$accountName/repos/GAM-boarding-scripts/dependencies/signature.txt
-
-(
-    #Sets path of GAM
-    GAM3=/Users/$accountName/bin/gamadv-xtd3/gam
-    echo "========================================"
-    echo "GAM3 command alias set to ${GAM3}"
-    echo
-    echo
-
+    echo "You ran the script without adequate arguments..."
+    echo ""
     echo "Input the FIRST NAME of the new user to be provisioned in Google Workspace, followed by [ENTER]"
     read onboard_first_name
-    echo
-    echo
-
+    echo ""
+    echo ""
     echo "Input the LAST NAME of the new user to be provisioned in Google Workspace, followed by [ENTER]"
     read onboard_last_name
-    echo
-    echo
-
+    echo ""
+    echo ""
     echo "Input the PERSONAL RECOVERY EMAIL of the new user to be provisioned in Google Workspace, followed by [ENTER]"
     read recovery_email
-    echo
-    echo
-
-    echo "Input the HUMAN RESOURCES EMAIL to be notify of the newly created user, followed by [ENTER]"
-    read hr_email
-    echo
-    echo
-
+    echo ""
+    echo ""
     echo "Input the WORK EMAIL of the new user to be provisioned in Google Workspace, followed by [ENTER]"
-    read onboard_email_address
-    echo
-    echo
-
-    echo "Input the email address of the new user's Manager, followed by [ENTER]"
+    read onboard_user
+    echo ""
+    echo ""
+    echo "Input the employee's JOB TITLE, followed by [ENTER]"
+    read job_title
+    echo ""
+    echo ""
+    echo "Input the email address of the new user's MANAGER, followed by [ENTER]"
     read manager_email_address
-    echo
-    echo
+    echo ""
+    echo ""
+fi
 
-    #Create new user, generate password, notify recovery email and hiring manager, set OU to "New users"
-    echo
-    echo "Creating new user..."
-    ${GAM3} create user $onboard_email_address firstname $onboard_first_name lastname $onboard_last_name org New\ users notify $recovery_email,$hr_email subject "[ACTION REQUIRED] Activate your #email# email" password "{{EXAMPLE_PASS_HERE}}" notifypassword "{{EMAILED_PASS_HINT}}" changepasswordatnextlogin
-    ${GAM3} update user $offboard_user Employment_History.Start_dates multivalued $NOW #https://github.com/GAM-team/GAM/wiki/GAM3DirectoryCommands#setting-custom-user-schema-fields-at-create-or-update
-    echo
-    echo
+confirm_inputs() {
+    echo "Employee: ${onboard_first_name} ${onboard_first_name} ${onboard_user}"
+    echo "Job title: ${job_title}"
+    echo "Manager: ${manager_email_address}"
+    wait 2
 
-    #Update org directory info
-    echo "What type of employee is this?"
-    echo " Admin"
-    echo " Staff"
-    echo " Fellow"
-    echo " Non-staff (MOU, seconded, lay leader, etc.)"
-    read type_of_employee
-    echo
-    echo
+    read -p "Press any key to continue... " -n1 -s
+    echo ""
+}
 
-    echo "What is the employee's reporting campus?"
-    echo " AND"
-    echo " SW"
-    echo " CRK"
-    echo " MT"
-    echo " SYS"
-    echo " KK"
-    read cost_center
-    echo
-    echo
-    #Add user to campus groups
-    case $cost_center in
-    AND)
-        echo "Adding user to Anderson campus staff email Groups, Calendar, and Drive."
-        ${GAM3} update group and-staff@grace-bible.org add member $onboard_email_address
+#Define available functions for the Whiptail menu
+start_logger() {
+    exec &> >(tee -a "$logFile")
+    echo "========================================"
+    echo "GAM3 command alias set to ${GAM3}"
+    ${GAM3} version
+    echo ""
+}
+
+set_password() {
+    echo ""
+    read -p "Do you want to set a custom temporary password? (Y/N) " yn
+
+    case $yn in
+    [yY])
+        read -p "Input the user temporary password, followed by [ENTER]: " TEMP_PASS
+        echo ""
         ;;
-    SW)
-        echo "Adding user to Southwood campus staff email Groups, Calendar, and Drive."
-        ${GAM3} update group sw-staff@grace-bible.org add member $onboard_email_address
-        ;;
-    CRK)
-        echo "Adding user to Creekside campus staff email Groups, Calendar, and Drive."
-        ${GAM3} update group crk-staff@grace-bible.org add member $onboard_email_address
-        ;;
-    MT)
-        echo "Adding user to Midtown campus staff email Groups, Calendar, and Drive."
-        ${GAM3} update group mt-staff@grace-bible.org add member $onboard_email_address
-        ;;
-    SYS)
-        echo "Adding user to System staff email Groups, Calendar, and Drive."
-        ${GAM3} update group sys-staff@grace-bible.org add member $onboard_email_address
-        ;;
-    KK)
-        #echo "Adding user to Kingdom Kids staff email Groups, Calendar, and Drive."
-        #${GAM3} update group  add member $onboard_email_address
-        echo "Kingdom Kids groups and resources haven't been clearly defined yet."
+    [nN])
+        echo "Proceeding with the default password: ${TEMP_PASS}"
+        echo ""
         ;;
     *)
-        echo
-        echo "Please enter a valid option."
-        echo
+        echo "Exiting..."
+        exit 1
         ;;
     esac
+}
 
+create_user() {
+    echo "Creating new user..."
+    ${GAM3} create user $onboard_user firstname $onboard_first_name lastname $onboard_last_name org New\ users notify $recovery_email,$onboard_manager subject "[ACTION REQUIRED] Activate your #email# email" password "${TEMP_PASS}" notifypassword "${TEMP_PASS}" changepasswordatnextlogin
+    ${GAM3} update user $onboard_user Employment_History.Start_dates multivalued $NOW
+    #https://github.com/GAM-team/GAM/wiki/GAM3DirectoryCommands#setting-custom-user-schema-fields-at-create-or-update
+    echo ""
+    echo ""
+}
+
+employment_status() {
+    echo "What type of employee is this?"
+    echo "Options include..."
+    echo "Staff, Fellows, Mobilization, Non-staff (MOU, seconded, lay leader, etc.)"
+    echo ""
+    read type_of_employee
+    echo ""
+}
+
+set_campus() {
+    echo "What is the employee's reporting campus?"
+    echo "Options include..."
+    echo "AND, SW, CRK, MT, SYS, KK"
+    echo ""
+    read campus
+    echo ""
+}
+
+set_department() {
     echo "What is (are) the employee's department(s)?"
-    echo " Grace56"
-    echo " Youth"
-    echo " Youth Impact"
-    echo " College"
-    echo " Young Adults"
-    echo " Adult"
-    echo " Global Outreach"
-    echo " Mobilization"
+    echo "Please enter departments separated by commas and without any spaces"
+    echo "e.g Grace56, Youth, YI, College, Junction, GO, Mobilization, etc."
+    echo ""
     read department
-    echo
-    echo
+    echo ""
+}
 
-    echo "What is the employee's job title?"
-    read job_title
-    echo
-    echo
-
+update_info() {
     echo "Updating employee org info"
-    ${GAM3} update user $onboard_email_address relation manager $manager_email_address organization description "$type_of_employee" costcenter "$cost_center" department "$department" title "$job_title" primary
-    echo
-    echo
+    ${GAM3} update user $onboard_user relation manager $manager_email_address organization description "$type_of_employee" costcenter "$campus" department "$department" title "$job_title" primary
+    echo ""
+}
 
-    #Setting signature
+set_signature() {
     echo "Setting up Signature..."
-    ${GAM3} user $onboard_email_address signature file $sigFile replace NAME "$onboard_first_name $onboard_last_name" replace TITLE "$job_title"
-    echo
-    echo
+    ${GAM3} user $onboard_user signature file $sigFile replace NAME "$onboard_first_name $onboard_last_name" replace TITLE "$job_title"
+    echo ""
     echo "Here's the current signature of the user:"
-    ${GAM3} user $onboard_email_address show signature format
-    echo
-    echo
+    ${GAM3} user $onboard_user show signature format
+    echo ""
+}
 
-    #Define error handling for admin verification
-    invalidUsernameString="Does not exist"
-    validUsernameString="User"
+add_groups() {
+    ${GAM3} update group ${campus}-${type_of_employee}@grace-bible.org add member $onboard_user
+    echo "Adding user to ${campus} campus ${type_of_employee} email Groups, Calendar, and Drive."
+    echo ""
+}
 
-    #Verifies admin email address & downloads the new hire tracker sheet from google drive
-    echo "To complete onboarding and upload logs, verify your admin email address."
-    while [ "$verifyAdmin" != *"$invalidUsernameString"* ]; do
-        read -p "Admin Email address: " adminName
-        echo
-        echo "Validating email address..."
-        verifyAdmin=$(${GAM3} whatis $adminName noinfo 2>&1)
-        if [[ "$verifyAdmin" != *"$validUsernameString"* ]]; then
-            echo "Invalid Email address, try again."
-        fi
+get_info() {
+    echo "Logging newly onboarded user's info for audit..."
+    ${GAM3} info user $onboard_user
+    echo ""
+}
 
-        if [[ "$verifyAdmin" == *"$validUsernameString"* ]]; then
-            echo
-            echo "Administrator verified"
-            echo
-            # Please make sure to change the id below to match your excel or google sheet file
-            # ${GAM3} user $adminName get drivefile id {{INSERT GOOGLE DRIVE FILE ID HERE}} format microsoft targetfolder /Users/$accountName/GAMWork targetname newHireTracker.xlsx overwrite true
-            break
-        fi
-    done
-
-    #Uploading logs to IT subcommittee Team Drive folder for audit and reference
-    echo "Initiating saving of logs"
-    echo
-    echo "User is provisioned in Google Workspace by $adminName at $NOW"
-    #Make sure to change the folder id of your g-drive below
-    #${GAM3} user $adminName add drivefile localfile $logLocation teamdriveparentid 1Gb2n_u9KD5AMQ3EunO51pUE44SKTnoxm
-    echo
-    echo "Locally, you can find the log at: "
-    echo $logLocation
-    echo
-
-    ${GAM3} info user $onboard_email_address
-
-    echo "Google Workspace provisioning for $onboard_email_address complete"
-    echo "Emailed credentials to $recovery_email and $hr_email"
+end_logger() {
+    echo "Google Workspace boarding process complete"
+    echo "Emailed credentials to $recovery_email and $onboard_manager"
+    echo "Locally, you can find the log at: ${logFile}"
+    echo ""
     echo "========================================"
+}
 
-    #redirect stdout/stderr to a file
-) 2>&1 | tee -a "$logLocation"
+start_logger
+set_password
+create_user
+employment_status
+set_campus
+set_department
+update_info
+set_signature
+add_groups
+get_info
+end_logger
 
 cd $INITIAL_WORKING_DIRECTORY
