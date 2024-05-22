bac2a9757bc58143a46207a5166d4ec9536b8242
Josh McKenna <joshmckenna@grace-bible.org>
2024-05-22 17:50:46 -0500

    Add more user feedback for clarity

--- offboard.sh
+++ offboard.sh
@@ -114,109 +114,184 @@ confirm_inputs
 confirm_continue
 
 unsuspend() {
+    echo "Entering unsuspend function at $(date)"
     echo "Unsuspending user account for offboarding..."
     ${GAM3} update user $offboard_user suspended off
     echo ""
     echo "Waiting for suspension to be removed..."
     sleep 10
+    echo "Ready to continue!"
+    echo "Exiting unsuspend function at $(date)"
+    echo ""
     echo ""
 }
 
 get_info() {
-    echo "Logging user's pre-offboarding info for audit..."
+    echo "Entering get_info function at $(date)"
+    echo "Logging ${offboard_user}'s pre-offboarding info for audit..."
     ${GAM3} info user $offboard_user
+    echo "Showing email forwards for ${offboard_user}..."
     ${GAM3} user $offboard_user show forwards
+    echo "Showing Shared Drives for ${offboard_user}..."
+    ${GAM3} user $offboard_user show teamdrives
+    echo "${offboard_user}'s pre-offboarding info logged."
+    echo "Exiting get_info function at $(date)"
+    echo ""
     echo ""
 }
 
 reset_password() {
+    echo "Entering reset_password function at $(date)"
     echo "Generating random password..."
     ${GAM3} update user $offboard_user password random
+    echo "Requiring password change on next login..."
     ${GAM3} update user $offboard_user changepassword on
     sleep 2
     ${GAM3} update user $offboard_user changepassword off
+    echo "${offboard_user}'s password changed."
+    echo "Exiting reset_password function at $(date)"
+    echo ""
     echo ""
 }
 
 reset_recovery() {
+    echo "Entering reset_recovery function at $(date)"
     echo "Erasing recovery options for $offboard_user..."
     ${GAM3} update user $offboard_user recoveryemail "" recoveryphone ""
+    echo "Exiting reset_recovery function at $(date)"
+    echo ""
     echo ""
 }
 
 set_endDate() {
-    echo "Setting $offboard_user end date to today..."
+    echo "Entering set_endDate function at $(date)"
+    echo "Setting ${offboard_user} end date to today..."
     ${GAM3} update user $offboard_user Employment_History.End_dates multivalued $NOW
+    echo "${offboard_user}'s end date set."
+    echo "Exiting set_endDate function at $(date)"
+    echo ""
     echo ""
     #https://github.com/GAM-team/GAM/wiki/GAM3DirectoryCommands#setting-custom-user-schema-fields-at-create-or-update
 }
 
 deprovision() {
+    echo "Entering deprovision function at $(date)"
     echo "Deprovisioning application passwords, backup verification codes, and access tokens..."
     echo "Disabling POP/IMAP access, signing out all devices, and turning off MFA..."
     ${GAM3} user $offboard_user deprovision popimap signout turnoff2sv
+    echo "Generating new MFA backup codes..."
     ${GAM3} user $offboard_user update backupcodes
+    echo "${offboard_user} has been deprovisioned."
+    echo "Exiting deprovision function at $(date)"
+    echo ""
     echo ""
     #https://github.com/taers232c/GAMADV-XTD3/wiki/Users-Deprovision
 }
 
 remove_directory() {
-    echo "Hiding $offboard_user from the Global Address List (GAL)..."
+    echo "Entering remove_directory function at $(date)"
+    echo "Hiding ${offboard_user} from the Global Address List (GAL)..."
     ${GAM3} update user $offboard_user gal off
+    echo "${offboard_user} has been removed from the Global Address List (GAL)."
+    echo "Exiting remove_directory function at $(date)"
+    echo ""
     echo ""
     #https://github.com/GAM-team/GAM/wiki/GAM3DirectoryCommands
 }
 
 forward_emails() {
+    echo "Entering forward_emails function at $(date)"
     echo "Forwarding emails, granting delegate access to manager..."
     ${GAM3} user $offboard_user print forwardingaddresses | ${GAM3} csv - gam user "~User" delete forwardingaddress "~forwardingEmail"
+    echo "User-configured forwarding for ${offboard_user} has been deleted..."
     ${GAM3} user $offboard_user add forwardingaddress $receiving_user
+    echo "Email for ${offboard_user} has been forwarded to ${receiving_user}..."
     ${GAM3} user $offboard_user forward on $receiving_user archive
+    echo "Set ${offboard_user} inbox to archive after forwarding..."
     ${GAM3} user $offboard_user delegate to $receiving_user
+    echo "Email for ${offboard_user} has been delegated to ${receiving_user}..."
+    echo "Deleting ${offboard_user}'s email aliases..."
+    ${GAM3} user $offboard_user delete aliases
+    echo "${offboard_user}'s email aliases were deleted."
+    echo "Exiting forward_emails function at $(date)"
+    echo ""
     echo ""
     #https://github.com/taers232c/GAMADV-XTD3/wiki/Users-Gmail-Forwarding
 }
 
 set_autoreply() {
+    echo "Entering set_autoreply function at $(date)"
     echo "Configuring email autoreply..."
     company="Grace Bible Church"
     subject="No longer at $company"
+    echo "Subject: ${subject}"
     message="Thank you for contacting me. I am no longer working at $company. Please direct any future correspondence to $receiving_user."
+    echo "Message: ${message}"
     startDate=$NOW
     endDate=$(date -v +1y +%F)
+    echo "Autoreply set until: ${endDate}"
     ${GAM3} user $offboard_user vacation on subject "$subject" message "$message" startdate "$startDate" enddate "$endDate"
+    echo "Email autoreply for ${offboard_user} set."
+    echo "Exiting set_autoreply function at $(date)"
+    echo ""
     echo ""
 }
 
 transfer_drive() {
+    echo "Entering transfer_drive function at $(date)"
     echo "Transferring Drive..."
     ${GAM3} user $offboard_user transfer drive $receiving_user
     ${GAM3} create datatransfer $offboard_user gdrive $receiving_user
+    echo "${offboard_user}'s My Drive was transferred to ${receiving_user}"
+    echo "Exiting transfer_drive function at $(date)"
+    echo ""
     echo ""
     #https://github.com/taers232c/GAMADV-XTD3/wiki/Users-Drive-Transfer
 }
 
 transfer_calendar() {
+    echo "Entering transfer_calendar function at $(date)"
     echo "Transferring Calendar..."
     ${GAM3} calendar $offboard_user add owner $receiving_user
     ${GAM3} add datatransfer $offboard_user calendar $receiving_user releaseresources
+    echo "${offboard_user}'s calendar was transferred to ${receiving_user}"
+    echo "Exiting transfer_calendar function at $(date)"
+    echo ""
     echo ""
 }
 
 remove_groups() {
+    echo "Entering remove_groups function at $(date)"
     echo "Removing from groups..."
     ${GAM3} user $offboard_user delete groups
-    echo
+    echo "${offboard_user} removed from groups."
+    echo "Exiting remove_groups function at $(date)"
+    echo ""
+    echo ""
 }
 
 remove_drives() {
+    echo "Entering remove_drives function at $(date)"
+    echo "Removing from Shared Drives..."
     ${GAM3} redirect csv ./SharedDriveAccess.csv multiprocess user $offboard_user print shareddrives fields id,name
     ${GAM3} redirect stdout ./DeleteSharedDriveAccess.txt multiprocess redirect stderr stdout csv ./SharedDriveAccess.csv gam delete drivefileacl ~~id~~ ~~User~~
+    echo "${offboard_user} removed from Shared Drives."
+    echo "Cleaning up temporary files..."
+    rm ./SharedDriveAccess.csv
+    rm ./DeleteSharedDriveAccess.txt
+    echo "Temporary files deleted."
+    echo "Exiting remove_drives function at $(date)"
+    echo ""
+    echo ""
 }
 
 set_org_unit() {
+    echo "Entering set_org_unit function at $(date)"
     echo "Moving $offboard_user to offboarding OU..."
     ${GAM3} update org 'Inactive' move user $offboard_user
+    echo "${offboard_user} moved to Inactive OU."
+    echo "Exiting set_org_unit function at $(date)"
+    echo ""
     echo ""
 }
 
@@ -224,15 +299,14 @@ suspend() {
     echo "Waiting for previous changes to take effect..."
     sleep 10
     ${GAM3} update user $offboard_user suspended on
-}
-
-cleanup_tmp_files() {
-    rm ./SharedDriveAccess.csv
-    rm ./DeleteSharedDriveAccess.txt
+    echo "${offboard_user} was suspended."
+    echo ""
+    echo ""
 }
 
 end_logger() {
     echo "Google Workspace boarding process complete"
+    echo ""
     echo "========================================"
 }
 
@@ -321,7 +395,6 @@ suspend
 end_logger
 
 #Return to the pre-script working directory
-cleanup_tmp_files
 cd $INITIAL_WORKING_DIRECTORY
 
 #Heavily inspired by Sean Young's [deprovision.sh](https://github.com/seanism/IT/tree/5795238dc1309f245d939c89e975c805dda745f3/GAM)
