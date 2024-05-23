1c9ae10d73814ebeb7d2f9f7fd773db42f2562ab
Josh McKenna <joshmckenna@grace-bible.org>
2024-05-23 12:59:31 -0500

    Add latest update check for GAM and GAMADV-XTD3

--- print.sh
+++ print.sh
@@ -2,16 +2,42 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-# Update GAM
-bash <(curl -s -S -L https://gam-shortn.appspot.com/gam-install) -l
-
-# Update GAMADV-XTD3
-bash <(curl -s -S -L https://raw.githubusercontent.com/taers232c/GAMADV-XTD3/master/src/gam-install.sh) -l
-
 source "$(dirname "$0")/config.env"
 
-# Get the current date in the YYYY-MM-DD format
+# Initialize variables
+NOW=$(date '+%F')
+logFile=${LOG_DIR}/$NOW.log
 date_prefix=$(date '+%Y-%m-%d %H%M')
+
+# Function to update GAM and GAMADV-XTD3
+update_gam() {
+    echo "Updating GAM and GAMADV-XTD3..."
+    bash <(curl -s -S -L https://gam-shortn.appspot.com/gam-install) -l
+    bash <(curl -s -S -L https://raw.githubusercontent.com/taers232c/GAMADV-XTD3/master/src/gam-install.sh) -l
+    # Update the last update date in the config.env file
+    sed -i'' -e "s/^GAM_LAST_UPDATE=.*/GAM_LAST_UPDATE=\"${NOW}\"/" "$(dirname "$0")/config.env"
+    export GAM_LAST_UPDATE="${NOW}"
+}
+
+# Check the last update date
+if [[ -z "${GAM_LAST_UPDATE:-}" ]]; then
+    echo "GAM_LAST_UPDATE variable is not set in the config file."
+    update_gam
+else
+    LAST_UPDATE_DATE=$(date -j -f "%Y-%m-%d" "${GAM_LAST_UPDATE}" "+%s")
+    CURRENT_DATE_SECS=$(date -j -f "%Y-%m-%d" "${NOW}" "+%s")
+    SECONDS_DIFF=$((CURRENT_DATE_SECS - LAST_UPDATE_DATE))
+    DAYS_SINCE_LAST_UPDATE=$((SECONDS_DIFF / 86400))
+
+    if [ "${DAYS_SINCE_LAST_UPDATE}" -ge "${UPDATE_INTERVAL_DAYS}" ]; then
+        update_gam
+    else
+        echo "GAM was updated ${DAYS_SINCE_LAST_UPDATE} days ago. Skipping update."
+    fi
+fi
+
+# Ensure the log directory exists
+mkdir -p "${LOG_DIR}"
 
 # Define the output file names with the date prefix
 orgs_file="${date_prefix} gam orgs.csv"
